903c77cb59cdc12b2aaf1ed0e6185985dd34b20b
shahbozdeveloper <germanationit11@gmail.com>
2021-09-29 14:51:17 +0500

    'start_bootstrap'

--- start_bootstrap/config/admin.py
+++ start_bootstrap/config/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import bootstrap_user
 # Register your models here.
+
+class bootstrap_user_admin(admin.ModelAdmin):
+    list_display = ['name','discription']
+
+admin.site.register(bootstrap_user,bootstrap_user_admin)
